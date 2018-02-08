f94d68cd126ac0139c595be608ed8bc656bf03bc
Darius <2265351d@student.gla.ac.uk>
2018-02-08 15:03:14 +0000

    chapter 6

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,8 @@ from django.http import HttpResponse
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,'pages':page_list}
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     return render(request, 'rango/about.html')
@@ -24,4 +25,5 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
-    
+
+
